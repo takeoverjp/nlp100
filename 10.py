f30947298baa4d360f06ca5320ef80cb743cdee2
Kondo Takeo <takeoverjp@gmail.com>
2019-08-26 12:24:18 +0900

    Add 10.py.

---
+++ 10.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+
+if len(sys.argv) != 2:
+  sys.exit("USAGE: " + sys.argv[0] + " DATA_FILE")
+
+DATA_FILE=sys.argv[1]
+
+count=0
+with open(sys.argv[1], "r") as f:
+  for line in f.readlines():
+    count += 1
+
+print(count)
